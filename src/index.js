750da392195c0ad740f403ec904da7a6ebe41981
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-14 01:27:49 +0200

    u

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-// import calcSum from "./js/calc-sum";
-// const calc = calcSum;
-// console.log(calcSum);
-// const result = calc(10, 2);
-// console.log(result);
-console.log("hello");
+// -----------------  дефолтний імпорт    ---------------------- //
+
+// import calc from "./js/calc-sum";
+// const {calcSum, calcMultipl, calcSub, calcDiv} = calc;
+
+// const x = 100;
+// const y = 5;
+
+// const r1 = calcSum(x, y);
+// const r2 = calcMultipl(x, y);
+// const r3 = calcSub(x, y);
+// const r4 = calcDiv(x, y);
+
+// console.log(r1, r2, r3, r4);
+
+// -----------------  /дефолтний імпорт    ---------------------- //
+
+// // -----------------  іменований експорт    ---------------------- //
+// import {calcSum, calcMultipl, calcSub, calcDiv} from "./js/calc-sum";
+
+// const x = 100;
+// const y = 5;
+
+// const r1 = calcSum(x, y);
+// const r2 = calcMultipl(x, y);
+// const r3 = calcSub(x, y);
+// const r4 = calcDiv(x, y);
+
+// console.log(r1, r2, r3, r4);
+// // -----------------  /іменований експорт    ---------------------- //
+
+// -----------------  namespace import    ---------------------- //
+import * as calc from "./js/calc-sum";
+const {calcSum, calcMultipl, calcSub, calcDiv} = calc;
+
+const x = 100;
+const y = 5;
+
+const r1 = calcSum(x, y);
+const r2 = calcMultipl(x, y);
+const r3 = calcSub(x, y);
+const r4 = calcDiv(x, y);
+
+console.log(r1, r2, r3, r4);
+// -----------------  /namespace import    ---------------------- //
--- src/js/calc-sum.js
+++ src/js/calc-sum.js
@@ -1,4 +1,35 @@
-export default function calcSum(a, b) {
+// -----------------  дефолтний експорт    ---------------------- //
+
+// function calcSum(a, b) {
+//   return a + b;
+// }
+
+// function calcMultipl(a, b) {
+//   return a * b;
+// }
+// function calcSub(a, b) {
+//   return a - b;
+// }
+// function calcDiv(a, b) {
+//   return a / b;
+// }
+
+// export default { calcSum, calcMultipl, calcSub, calcDiv };
+// -----------------  /дефолтний експорт    ---------------------- //
+
+// -----------------  іменований експорт    ---------------------- //
+export const calcSum = (a, b) => {
   return a + b;
-}
-calcSum(a, b);
+};
+
+export const calcMultipl = (a, b) => {
+  return a * b;
+};
+export const calcSub = (a, b) => {
+  return a - b;
+};
+export const calcDiv = (a, b) => {
+  return a / b;
+};
+
+// -----------------  /іменований експорт    ---------------------- //
